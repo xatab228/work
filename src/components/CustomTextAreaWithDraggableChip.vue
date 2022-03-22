0753b1cbad8f02f6b65d09a02aed6a584d95966a
xatab228 <xatab228@gmail.com>
2022-03-22 19:34:00 +0300

    #1 Погнали нахуй

---
+++ src/components/CustomTextAreaWithDraggableChip.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "CustomTextAreaWithDraggableChip"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/TestsPlaces.vue
+++ src/views/TestsPlaces.vue
@@ -1,15 +1,17 @@
 <template>
  <div class="tests-places">
    <p>Place for KALLLLLLLLLLLLL</p>
-
-
-
+    <div>
+      <custom-text-area-with-draggable-chip/>
+    </div>
  </div>
 </template>
 
 <script>
+import CustomTextAreaWithDraggableChip from "@/components/CustomTextAreaWithDraggableChip";
 export default {
-  name: "TestsPlaces"
+  name: "TestsPlaces",
+  components: {CustomTextAreaWithDraggableChip}
 }
 </script>
 
