1fc0467ec07e64f2ae76385fd5eed4c15b01019e
xatab228 <xatab228@gmail.com>
2022-03-23 00:02:08 +0300

    #1 Погнали нахуй

---
+++ src/components/SelectedSort.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <v-select
+          v-model="selectedOption"
+          :items="options"
+          item-text="text"
+          item-value="value"
+          @change="optionChange"
+      >
+
+      </v-select>
+      <div v-for="item in data" :key="item.id">
+        {{item.name}}
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectedSort.vue",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    options: {
+      type: Array,
+      default: () => []
+    },
+    select: [String]
+  },
+  data: () => ({
+    selectedOption: this.select
+  }),
+  methods: {
+    optionChange(){
+      console.log(this.selectedOption)
+      this.$emit('update:select',this.selectedOption)
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/TestsPlaces.vue
+++ src/views/TestsPlaces.vue
@@ -3,15 +3,41 @@
    <p>Place for KALLLLLLLLLLLLL</p>
     <div>
       <custom-text-area-with-draggable-chip/>
+      <selected-sort :data="data" :options="options" :select.sync="selectOption"/>
+      {{selectOption}}
     </div>
  </div>
 </template>
 
 <script>
 import CustomTextAreaWithDraggableChip from "@/components/CustomTextAreaWithDraggableChip";
+import SelectedSort from "@/components/SelectedSort";
+import axios from 'axios'
 export default {
   name: "TestsPlaces",
-  components: {CustomTextAreaWithDraggableChip}
+  components: {CustomTextAreaWithDraggableChip, SelectedSort},
+  data: () => ({
+    data: [],
+    options: [
+      { value: "title", text: "Name" },
+      { value: "body", text: "Content" },
+    ],
+    selectOption: '',
+  }),
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        this.data = [...this.data,...response.data]
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
+  created() {
+    this.fetchData()
+  }
+
 }
 </script>
 
