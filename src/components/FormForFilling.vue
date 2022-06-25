9a5b67a82704789ed322c190c83c1c22a64ca74a
xatab228 <xatab228@gmail.com>
2022-06-25 16:49:20 +0300

    #1 Create plugins and eslint

---
+++ src/components/FormForFilling.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="form-for-filling">
+		<div class="form-for-filling__header">
+			<span class="title"> POshel Nahuy </span>
+		</div>
+		<v-divider/>
+		<div class="form-for-filling__body">
+		</div>
+		<v-divider/>
+		<div class="form-for-filling__footer">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "FormForFilling"
+}
+</script>
+
+<style lang="scss">
+.form-for-filling {
+	display: grid;
+	grid-template-rows: 1fr 16fr 1fr;
+	width: 100%;
+	padding: 10px 20px;
+	min-width: 450px;
+	min-height: 600px;
+	
+	&__header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	&__footer {
+		margin-left: auto;
+	}
+	&__body {
+	
+	}
+}
+</style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="modal-window" v-if="visible" @click="hideDialog">
+		<div class="modal-window__content">
+			<slot/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ModalWindow",
+	props: {
+		data: [Object]
+	},
+	data(){
+		return {
+			visible: false
+		}
+	},
+	mounted() {
+		this.visible = true
+	},
+	methods: {
+		hideDialog() {
+			console.log(this.data)
+			this.$emit('destroy');
+			this.visible = false
+		},
+	},
+}
+</script>
+
+<style lang="scss">
+.modal-window {
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background: rgba(0, 0, 1, 0.5);
+	position: fixed;
+	display: flex;
+	
+	&__content {
+		margin: auto;
+		background: white;
+		border-radius: 12px;
+		min-width: 200px;
+		min-height: 50px;
+		
+	}
+}
+</style>
